0413e62cfe1b99fc79d27d84c070cac811b92d04
adriannebulao <amcbulao@gmail.com>
2024-06-02 18:48:37 +0800

    add logic for basic operations

---
+++ script.js
@@ -0,0 +1,17 @@
+const add = (num1, num2) => num1 + num2;
+const subtract = (num1, num2) => num1 - num2;
+const multiply = (num1, num2) => num1 * num2;
+const divide = (num1, num2) => num1 / num2;
+
+const operate = (num1, num2, operation) => {
+  switch (operation) {
+    case "+":
+      return add(num1, num2);
+    case "-":
+      return subtract(num1, num2);
+    case "*":
+      return multiply(num1, num2);
+    case "/":
+      return divide(num1, num2);
+  }
+};
